d5a6a4f2bd3aa39427bbe2d477466d79a0d9954d
Sander Deneyer <sander.deneyer@student.odisee.be>
2021-10-13 20:55:55 +0200

    Creating with API requests (Requests)

---
+++ Recipe API/Recipe API/Migrations/20211013150908_Test.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipe_API.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Ingredients",
+                columns: new[] { "Id", "Name", "Quantity", "RecipeId", "Unit" },
+                values: new object[,]
+                {
+                    { 1, "Scampi's", 500.0, 1, "gram" },
+                    { 2, "Tomaten", 4.0, 1, null },
+                    { 3, "Look", 1.0, 1, "teen" },
+                    { 4, "Room", 1.0, 1, "liter" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Recipes",
+                columns: new[] { "Id", "CategoryId", "Dificulty", "Time", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 0, 45, "Scampi soupje" },
+                    { 2, 2, 2, 60, "Pasta Diablo" },
+                    { 3, 2, 2, 60, "Pasta Test" },
+                    { 4, 2, 2, 60, "Pasta Test2" },
+                    { 5, 2, 2, 60, "Pasta Test3" },
+                    { 6, 3, 1, 15, "Framboze ijsje" },
+                    { 7, 1, 1, 35, "Pasta 1" }
+                });
+        }
+    }
+}
