53509c0d84075023c93d5511b166195b9c59894d
Sander Deneyer <sander.deneyer@student.odisee.be>
2021-10-28 12:00:10 +0200

    Ingredient Controller & Service classe toevoegen aan C#

---
+++ Recipe API/Recipe API/Controllers/IngredientController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipe_API.Models;
+using Recipe_API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Recipe_API.Controllers
+{
+    [Route("api/ingredient")]
+    [ApiController]
+    public class IngredientController : ControllerBase
+    {
+        private readonly IngredientService _ingredientService;
+
+        public IngredientController(IngredientService ingredientService)
+        {
+            _ingredientService = ingredientService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Ingredient>> GetAllIngredients()
+        {
+            return _ingredientService.GetAll().ToList();
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<Ingredient> GetIngredient(int id)
+        {
+            var ingredient = _ingredientService.GetById(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return ingredient;
+        }
+
+        [HttpPost]
+        public ActionResult<Ingredient> CreateIngredient(Ingredient ingredient)
+        {
+            Ingredient created = _ingredientService.Create(ingredient);
+
+            return CreatedAtAction(nameof(GetIngredient), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Ingredient> DeleteIngredient(int id)
+        {
+            Ingredient ingredient = _ingredientService.GetById(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            _ingredientService.Delete(ingredient);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Recipe API/Recipe API/Services/IngredientService.cs
@@ -0,0 +1,45 @@
+﻿using Recipe_API.Data;
+using Recipe_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_API.Services
+{
+    public class IngredientService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RecipeService _recipeService;
+
+        public IngredientService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Ingredient> GetAll()
+        {
+            return _context.Ingredients.ToList();
+        }
+
+        public Ingredient GetById(int id)
+        {
+            return _context.Ingredients.FirstOrDefault(x => x.Id == id);
+        }
+        
+        public Ingredient Create(Ingredient ingredient)
+        {
+            _context.Ingredients.Add(ingredient);
+            _context.SaveChanges();
+
+            return ingredient;
+        }
+
+        public void Delete(Ingredient ingredient)
+        {
+            _context.Ingredients.Remove(ingredient);
+            _context.SaveChanges();
+        }        
+    }
+}
+
